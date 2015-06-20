1322459d8f00b595fc0a731c6a474e5a1b6c97f1
Johnny Lin <johnny@tailwindweb.com>
2015-06-20 16:20:21 -0400

    added overlaying animation on scroll

---
+++ js/application.js
@@ -0,0 +1,33 @@
+(function() {
+    $(document).ready(function() {
+        var sectionManager = new SectionManager();
+        sectionManager.setSections();
+        $(window).scroll(function() {
+            sectionManager.setSections();
+        });
+        $(window).resize(function() {
+            sectionManager.setSections();
+        })
+    });
+
+    function SectionManager() {
+        var self = this;
+        this.currentSection = null;
+
+        this.setSections = function() {
+            var scrollPosition = $(window).scrollTop();
+            $('section').each(function() {
+                var sectionTop = $(this).offset().top;
+                var sectionBottom = $(this).offset().top + $(this).height();
+                if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                    if (self.currentSection !== $(this).attr('id')) {
+                        $('.current').removeClass('current');
+                        self.currentSection = $(this).attr('id');
+                        $('#'+self.currentSection).addClass('current');
+                    }
+                    return false;
+                }
+            });
+        };
+    }
+}());
