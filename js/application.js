a9ea4c9cfdb6e2d794c15c408f627576b99fbf13
Johnny Lin <johnny@tailwindweb.com>
2015-06-27 11:47:00 -0400

    added background scaling when scrolling

--- js/application.js
+++ js/application.js
@@ -1,4 +1,9 @@
 (function() {
+    var CONSTANTS = {
+        CURRENT_SCALE_MIN: 1,
+        CURRENT_SCALE_MAX: 1.1
+    };
+
     var secondJobs = [
         "Party Ruiner",
         "Sock Model",
@@ -32,6 +37,8 @@
                 var sectionTop = $(this).offset().top;
                 var sectionBottom = $(this).offset().top + $(this).height();
                 if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                    var backgroundScale = (((scrollPosition - sectionTop) / (sectionBottom - sectionTop)) * (CONSTANTS.CURRENT_SCALE_MAX - CONSTANTS.CURRENT_SCALE_MIN)) + CONSTANTS.CURRENT_SCALE_MIN;
+                    $(this).find('.background').css('transform', 'translate3d(0, 0, 0) scale(' + backgroundScale + ')');
                     if (self.currentSection !== $(this).attr('id')) {
                         $('.current').removeClass('current');
                         self.currentSection = $(this).attr('id');
