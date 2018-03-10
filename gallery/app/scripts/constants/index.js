8a1c2d8f5ffa31f5e4a9fd1c36b8c1672f3d50c7
Johnny Lin <thejohnnylin@gmail.com>
2018-03-10 17:53:06 -0500

    add Harry's

--- gallery/app/scripts/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const IMAGE_DIR = `${window.location.origin}${window.location.pathname}app/images/`;
-
-// Determined by react-hammerjs library
-export const SWIPE_LEFT = 2;
-export const SWIPE_RIGHT = 4;
